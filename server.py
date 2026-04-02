9865b17899642e7f97a708b7b53ed3ee844c039f
Ignacio Gomez <igomez9@dons.usfca.edu>
2026-04-02 13:07:14 -0700

    Refactor code structure for improved readability and maintainability

--- server.py
+++ server.py
@@ -14,6 +14,32 @@ def hello() -> dict:
     return {"message": "Welcome to mlops fastapi!"}
 
 
+@app.get("/usfca")
+def usfca() -> str:
+    print("someone requested the usfca endpoint")
+    return "something"
+
+
+@app.post("/add_query_parameters")
+def addWithQueryParameters(num1: int, num2: int) -> dict:
+    result = num1 + num2
+    return {"result": result}
+
+
+# add with body parameters
+@app.post("/add_body_parameters")
+def addWithBodyParameters(request: dict) -> dict:
+    num1 = int(request.get("num1"))
+    num2 = int(request.get("num2"))
+
+    b0 = 27
+    b1 = 256
+    b2 = 339
+    prediction = b0 + b1 * num1 + b2 * num2
+
+    return {"result": prediction}
+
+
 class PredictRequest(BaseModel):
     # Define your input data structure here
     data: str
