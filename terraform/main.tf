f1a34266e0aa9c10ea0834368a40bd84484d230b
Ignacio Gomez <igomez9@dons.usfca.edu>
2026-04-03 09:59:20 -0700

    Add Google Cloud Storage bucket resource for MLflow artifacts

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,3 +8,11 @@ resource "google_artifact_registry_repository" "mlflow" {
   location      = var.region
   format        = "DOCKER"
 }
+
+resource "google_storage_bucket" "mlflow_artifacts" {
+  name          = "${var.project_id}-mlflow-artifacts"
+  location      = var.region
+  force_destroy = false
+
+  uniform_bucket_level_access = true
+}
